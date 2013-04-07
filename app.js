2aa66088f7466153cd8f69cc892c63878cdaa792
palf <jack.palf@gmail.com>
2013-04-07 09:47:07 +0100

    simplified app to just 'hello world'

--- app.js
+++ app.js
@@ -1,38 +1,37 @@
-var express = require('express')
-  , routes = require('./routes')
-  , user = require('./routes/user')
-  , http = require('http')
-  , path = require('path');
+var express = require('express');
+var http = require('http')
 
 
-var app = express();
+var app = express()
+var port = process.env.PORT || 5000;
 
-app.set('port', process.env.PORT || 3000);
-app.set('views', __dirname + '/views');
-app.set('view engine', 'jade');
-app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.bodyParser());
-app.use(express.methodOverride());
-app.use(app.router);
-app.use(express.static(path.join(__dirname, 'public')));
+app.get('/', function(request, response) {
+  response.send('Hello World!');
+});
 
+http.createServer(app).listen(port, function() {
+  console.log("Listening on " + port);
+});
+
+
+
+// app.set('views', __dirname + '/views');
+// app.set('view engine', 'jade');
+// app.use(express.favicon());
+// app.use(express.logger('dev'));
+// app.use(express.bodyParser());
+// app.use(express.methodOverride());
+// app.use(app.router);
+// app.use(express.static(path.join(__dirname, 'public')));
+
+// // development only
 // if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
+//   app.use(express.errorHandler());
 // }
 
+// app.get('/', routes.index);
+// app.get('/users', user.list);
 
-
-// app.get('/', function (req, res) {
-//     res.sendfile(__dirname + '/index.html');
+// http.createServer(app).listen(app.get('port'), function(){
+//   console.log('Express server listening on port ' + app.get('port'));
 // });
-
-
-app.get('/', routes.index);
-app.get('/users', user.list);
-
-
-server = http.createServer(app)
-server.listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
-});
