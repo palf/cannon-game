0ee71646214f2a31cd8ef7ff20fc5ad2795e0bf3
palf <jack.palf@gmail.com>
2013-04-08 00:04:35 +0100

    added instructions to index page

--- app.js
+++ app.js
@@ -9,8 +9,10 @@ var port = process.env.PORT || 5000;
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
-// app.use(express.favicon());
-// app.use(express.logger('dev'));
+
+if ('development' == app.get('env')) {
+  app.use(express.logger('dev'));
+}
 // app.use(express.bodyParser());
 // app.use(express.methodOverride());
 // app.use(app.router);
